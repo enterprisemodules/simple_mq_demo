a0bb101c3125fd0e521f615037e056825b4821ef
Bert Hajee <bert.hajee@enterprisemodules.com>
2021-05-31 08:32:35 +0200

    Update to PE 2021.1 and fix PE setup

--- windows_hosts_file.pp
+++ windows_hosts_file.pp
@@ -1,4 +1,5 @@
 host { 'mqmaster.example.com': ip => '192.168.253.10', host_aliases => 'mqmaster' }
+host { 'mq90.example.com': ip => '192.168.253.30', host_aliases => 'mq90' }
 host { 'mq91.example.com': ip => '192.168.253.30', host_aliases => 'mq91' }
 host { 'mq91sol.example.com': ip => '192.168.253.31', host_aliases => 'mq91sol' }
 host { 'mqwindows.example.com': ip => '192.168.253.31', host_aliases => 'mqwindows' }
