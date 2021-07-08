fec20adc0124d3df7ccdde137a8f716a68050c7b
Bert Hajee <bert.hajee@enterprisemodules.com>
2021-07-08 14:23:49 +0200

    Add MQ 9.2 in the demo

--- windows_hosts_file.pp
+++ windows_hosts_file.pp
@@ -1,5 +1,6 @@
 host { 'mqmaster.example.com': ip => '192.168.253.10', host_aliases => 'mqmaster' }
 host { 'mq90.example.com': ip => '192.168.253.30', host_aliases => 'mq90' }
 host { 'mq91.example.com': ip => '192.168.253.30', host_aliases => 'mq91' }
-host { 'mq91sol.example.com': ip => '192.168.253.31', host_aliases => 'mq91sol' }
+host { 'mq92.example.com': ip => '192.168.253.31', host_aliases => 'mq92' }
+host { 'mq91sol.example.com': ip => '192.168.253.32', host_aliases => 'mq91sol' }
 host { 'mqwindows.example.com': ip => '192.168.253.31', host_aliases => 'mqwindows' }
