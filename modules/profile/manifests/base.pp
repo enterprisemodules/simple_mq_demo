a9cd96b4623ab34be1f34124ac3213fd31846ac1
Bert Hajee <bert.hajee@enterprisemodules.com>
2025-02-18 11:23:48 +0100

    Start using em plugin

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -1,6 +1,6 @@
 class profile::base()
 {
-  contain "profile::base::${::deployment_zone}"
+  contain "profile::base::${facts['deployment_zone']}"
   contain profile::base::config
   contain profile::base::hosts
   contain profile::base::packages
--- windows_hosts_file.pp
+++
@@ -1,6 +0,0 @@
-host { 'mqmaster.example.com': ip => '192.168.56.10', host_aliases => 'mqmaster' }
-host { 'mq90.example.com': ip => '192.168.56.30', host_aliases => 'mq90' }
-host { 'mq91.example.com': ip => '192.168.56.30', host_aliases => 'mq91' }
-host { 'mq92.example.com': ip => '192.168.56.31', host_aliases => 'mq92' }
-host { 'mq91sol.example.com': ip => '192.168.56.32', host_aliases => 'mq91sol' }
-host { 'mqwindows.example.com': ip => '192.168.56.31', host_aliases => 'mqwindows' }
